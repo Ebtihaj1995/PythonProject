607d4c7719f3bc07cbcc9e0d4c79f775ee8d87e1
Ebtihaj1995 <48633098+Ebtihaj1995@users.noreply.github.com>
2019-08-31 08:31:37 -0700

    Second commit
    This is second subject

---
+++ Second.py
@@ -0,0 +1,23 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> #this is comment
+... print("Hello,World")
+Hello,World
+>>> print("Hello,World")#this is comment
+Hello,World
+>>> #print("Hello,World")
+... print("Cheers,Mate!")
+Cheers,Mate!
+>>> """
+... """ This is a comment written in more than just one line """
+  File "<stdin>", line 2
+    """ This is a comment written in more than just one line """
+>>> """
+... This is a comment
+... written in
+... more than just one line
+... """
+'\nThis is a comment\nwritten in\nmore than just one line\n'
+>>> print("Hello, World!")
+Hello, World!
+>>>
