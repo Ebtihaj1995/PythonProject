ef17f5aa92cb9234733ec65f0a0df6790729259d
Ebtihaj1995 <48633098+Ebtihaj1995@users.noreply.github.com>
2019-09-02 08:10:12 -0700

    day 3

---
+++ day6.py
@@ -0,0 +1,35 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> x=int(1) # x will be 1
+>>> y=int(2.8) # y will be 2
+>>> z=int("3") 3 z will be 3
+  File "<stdin>", line 1
+>>> z=int("3") # z will be 3
+>>> print(x)
+1
+>>> print(y)
+2
+>>> print(z)
+3
+>>> x=float(1) #x will be 1.0
+>>> y=float(2.8) #y will be 2.8
+>>> z=float("3") # z will be 3.0
+>>> w=float("4.2") # w will be 4.2
+>>> print(x)
+1.0
+>>> print(y)
+2.8
+>>> print(z)
+3.0
+>>> print(w)
+4.2
+>>> x=str("sl") #x will be 'sl'
+>>> y=str(2) # y will be '2'
+>>> z=str(3.0) # z will be '3.0'
+>>> print(x)
+sl
+>>> print(y)
+2
+>>> print(z)
+3.0
+>>>
