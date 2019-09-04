15991b490318d64c9c93de2d0304df532659209b
Ebtihaj1995 <48633098+Ebtihaj1995@users.noreply.github.com>
2019-09-04 11:50:14 -0700

    Create day10.py

---
+++ day10.py
@@ -0,0 +1,40 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> x=5
+>>> y=3
+>>> print(x * y)
+15
+>>> print( x / y)
+1.6666666666666667
+>>> print(x%y)
+2
+>>> print(4 % 3)
+1
+>>> print(x ** y)
+125
+>>> print(x // y)
+1
+>>> print(4//3)
+1
+>>> print(3//3)
+1
+>>> x^=2
+>>> print(x)
+7
+>>> print(x+4)
+11
+>>> x=x>>3
+>>> print(x)
+0
+>>> x>>=3
+>>> print(x)
+0
+>>> x=5
+>>> x/=3
+>>> print(x)
+1.6666666666666667
+>>> x=5
+>>> y=3
+>>> print(x<y)
+False
+>>>
