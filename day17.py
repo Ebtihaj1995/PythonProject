1d4a374dbe4530da3707666ce8ccc6a522c0e3e8
Ebtihaj1995 <48633098+Ebtihaj1995@users.noreply.github.com>
2019-09-05 04:54:36 -0700

    Create day17.py

---
+++ day17.py
@@ -0,0 +1,31 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> thistuple =("apple", "banana","cherry")
+>>> if "apple" in thistuple:{
+... print("Yes,'apple' is in the fruits tuple") }
+...
+Yes,'apple' is in the fruits tuple
+{None}
+>>> thistuple=("Python") * 3
+>>> print(thistuple)
+PythonPythonPython
+>>> thistuple1=(1,2,3,4)
+>>> thistuple2=(5,6)
+>>> thistuple=thistuple1 + thistuple2
+>>> print(thistuple)
+(1, 2, 3, 4, 5, 6)
+>>> x=(3,4,5,6)
+>>> x=x+(1,2,3)
+>>> print(x)
+(3, 4, 5, 6, 1, 2, 3)
+>>> thistuple=("apple","banana","cherry")
+>>> print(len(thistuple))
+3
+>>> thistuple=tuple(("apple","banana","cherry"))
+>>> print(thistuple)
+('apple', 'banana', 'cherry')
+>>> thislist=[3,4,5,6,"A","B"]
+>>> thislist=tuple(thislist)
+>>> print(thistuple)
+('apple', 'banana', 'cherry')
+>>>
